5c1f750a024c2bd9c768587c62c2f78d50460bb8
Sahil Tandon <sahil.tandon@live.com>
2023-12-23 18:17:47 -0500

    React Sequence Board Game | Hand component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
 import Board from "../Board/Board";
+import Hand from "../Hand/Hand";
 
 const App = () => {
+  // Example data for testing the Hand component
+  const playerHand = [
+    { id: 101, value: "4" },
+    { id: 102, value: "5" },
+    { id: 103, value: "6" },
+    // Add more cards as needed
+  ];
+
   return (
     <div className="app">
       <h1>Sequence Game</h1>
       <Board />
+      <Hand cards={playerHand} />
       {/* Add other components as needed */}
     </div>
   );
