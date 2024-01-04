11ed04bb751bd31c595906248cecba29a6e25a33
Sahil Tandon <sahil.tandon@live.com>
2024-01-03 20:24:44 -0500

    React Sequence Board Game | JSDocs for Board.js

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,3 +1,11 @@
+/**
+ * Renders the game board.
+ *
+ * @param {Object} props - The component props.
+ * @param {Object} props.gameState - The game state object.
+ * @param {Function} props.onCardClick - The callback function for card click event.
+ * @returns {JSX.Element} The rendered game board.
+ */
 import "./Board.css";
 import Card from "../Card/Card";
 
